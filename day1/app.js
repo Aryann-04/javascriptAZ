673dc7fcfedf4d542d405496c1ba1841676b0715
Aryann-04 <aryanzalavadia4@gmail.com>
2025-03-03 11:11:47 +0530

    project 1

---
+++ day1/app.js
@@ -0,0 +1,114 @@
+// data type
+
+// let a = 156456;
+
+// let b = "ARYAN"
+
+// let c = true
+
+// let d = Symbol("jjkash" , 54464)
+
+// let e = BigInt(123145645646)
+
+// console.log("ARYAN");
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(e);
+
+// console.log(typeof(a));
+
+// console.log(typeof(b));
+
+// console.log(typeof(c));
+
+// console.log(typeof(d));
+
+// console.log(typeof(e));
+
+
+// operator
+
+// (1) assignment operator
+    // = , += , -= , = , /= , %= , *=
+
+    // let a = 7;
+
+    // console.log(a);
+
+    // a += 5;
+    // console.log(a);
+
+    // a -= 5;
+    // console.log(a);
+
+    // a *= 2;
+    // console.log(a);
+
+    // a /= 2;
+    // console.log(a);
+
+    // a %= 2;
+    // console.log(a);
+
+    // a **= 2;
+    // console.log(a);
+
+// (2) arithmatic operator
+    // // + , - , * , / , % , ++ , -- , **
+
+    // let x = 4;
+
+    // let y = 2;
+
+    // console.log(x + y);
+
+    // console.log(x - y);
+
+    // console.log(x * y);
+
+    // console.log(x / y);
+
+    // console.log(x % y);
+
+    // console.log(++x);
+
+    // console.log(--y);
+
+    // console.log(x ** y)
+
+//     let a = prompt("Enter 1 for DOG , 2 for CAT , 3 for COW , 4 for LION .")
+
+// if (a == 1) {
+//     console.log("🐶🐕")
+// } else if (a == 2) {
+//     console.log("😺🐈")
+// } else if (a == 3) {
+//     console.log("🐮🐄")
+// } else{
+//     console.log("🦁🦁")
+// }
+
+let a = prompt("Enter 1 for ARMANI EXCHANGE , 2 for DESEL , 3 for ZARA , 4 for RALPH LAUREN , 5 for US POLO , 6 for GANT.")
+
+if (a == 1) {
+    console.log("ARMANI EXCHANGE")
+} else if (a == 2) {
+    console.log("DESEL")
+} else if (a == 3) {
+    console.log("ZARA")
+} else if (a==4){
+    console.log("RALPH LAUREN")
+} else if (a==5){
+    console.log("US POLO")
+} else if (a==6){
+    console.log("GANT")
+}else{
+    console.log("❌")
+}
---
+++ projects JS/project 1/app.js
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------------QUE 1---------------------------------------------------------------------------
+
+let a = prompt("ENTER MARKS...")
+
+if (a >= 90 && a <= 100) {
+    console.log("A+")
+} else if (a >= 80 && a <= 89) {
+    console.log("A")
+} else if (a >= 70 && a <= 79) {
+    console.log("B")
+} else if (a >= 60 && a <= 69) {
+    console.log("C")
+} else if (a >= 50 && a <= 59) {
+    console.log("D")
+} 
+
+else (a >= 49); {
+    console.log("F")
+}
