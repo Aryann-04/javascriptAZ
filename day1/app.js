381f714ec59c09a24ef5c576c7aab53dcf6841d9
Aryann-04 <aryanzalavadia4@gmail.com>
2025-03-07 11:18:47 +0530

    AZ

--- day1/app.js
+++ day1/app.js
@@ -95,20 +95,120 @@
 //     console.log("🦁🦁")
 // }
 
-let a = prompt("Enter 1 for ARMANI EXCHANGE , 2 for DESEL , 3 for ZARA , 4 for RALPH LAUREN , 5 for US POLO , 6 for GANT.")
+// let a = prompt("Enter 1 for ARMANI EXCHANGE , 2 for DESEL , 3 for ZARA , 4 for RALPH LAUREN , 5 for US POLO , 6 for GANT.")
 
-if (a == 1) {
-    console.log("ARMANI EXCHANGE")
-} else if (a == 2) {
-    console.log("DESEL")
-} else if (a == 3) {
-    console.log("ZARA")
-} else if (a==4){
-    console.log("RALPH LAUREN")
-} else if (a==5){
-    console.log("US POLO")
-} else if (a==6){
-    console.log("GANT")
-}else{
-    console.log("❌")
+// if (a == 1) {
+//     console.log("ARMANI EXCHANGE")
+// } else if (a == 2) {
+//     console.log("DESEL")
+// } else if (a == 3) {
+//     console.log("ZARA")
+// } else if (a==4){
+//     console.log("RALPH LAUREN")
+// } else if (a==5){
+//     console.log("US POLO")
+// } else if (a==6){
+//     console.log("GANT")
+// }else{
+//     console.log("❌")
+// }
+// let age = 20;
+
+// if (age >= 18) {
+//     if (age <= 17) {
+//         console.log("You can drive.");
+//     } else {
+//         console.log("You can't drive.");
+//     }
+// } else {
+//     console.log("You are too young to drive.");
+// }
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+
+// if (a <= b) {
+//     if (a <= c) {
+//         console.log("a is min");
+        
+//     }else {
+//         console.log("c is min");
+        
+//     }
+// }else {
+//     if (b <= c) {
+//         console.log("b is min");
+//     }else {
+//         console.log("c is min");
+//     }
+// }
+
+
+// let a = 12;
+// let b = 35;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log ("a =", a, "b =", b);
+
+// let a = 12;
+// let b = 35;
+
+// [a,b] = [b,a];
+
+// console.log(a,b);
+
+
+let a = prompt("Enter A Chearter");
+
+
+switch (a) {
+    case "A":
+        alert("Vowels");
+        break;
+
+    case "E":
+        alert("Vowels");
+        break;
+
+    case "I":
+        alert("Vowels");
+        break;
+
+    case "O":
+        alert("Vowels");
+        break;
+
+    case "U":
+        alert("Vowels");
+        break;
+
+    case "a":
+        alert("Vowels");
+        break;
+
+    case "e":
+        alert("Vowels");
+        break;
+
+    case "i":
+        alert("Vowels");
+        break;
+
+    case "o":
+        alert("Vowels");
+        break;
+
+    case "u":
+        alert("Vowels");
+        break;
+
+    default :
+        alert ("Not vowels")
+    
+
 }
